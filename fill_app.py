49b2b0354d676fedc20ac3a4e62b326482baea10
mohammedhussainali1346 <aya1346@myyahoo.com>
2026-03-23 05:47:48 +0530

    Update fill_app.py

--- fill_app.py
+++ fill_app.py
@@ -1,5 +1,3 @@
-import os
-import gdown
 import streamlit as st
 import numpy as np
 import pickle
@@ -68,14 +66,7 @@ body {
 
 @st.cache_resource
 def load_resources():
-    model_path = "bigru_mask_model.h5"
-
-    if not os.path.exists(model_path):
-        file_id = "1rKqfaYB8lu53DBJudFnz2x9ldZUIbPo6"
-        url = f"https://drive.google.com/uc?id={file_id}"
-        gdown.download(url, model_path, quiet=False)
-
-    model = load_model(model_path)
+    model = load_model("bigru_mask_model.h5")
 
     with open("tokenizer.pkl", "rb") as f:
         tokenizer = pickle.load(f)
